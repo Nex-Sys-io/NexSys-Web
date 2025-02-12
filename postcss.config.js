9f7e151b1b259eafd83d08cfceee2c7f698415e3
PeterGQ <pandeve3@gmail.com>
2025-02-11 21:47:08 -0500

    first commit

--- postcss.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('postcss-load-config').Config} */
-module.exports = {
-  plugins: {
-    'postcss-import': {}, // Helps handle @import rules in CSS
-    'tailwindcss': {},    // Loads Tailwind CSS
-    'autoprefixer': {},   // Adds vendor prefixes automatically
-  },
-};
--- src/app/layout.server.js
+++
@@ -1,8 +0,0 @@
-import ClientLayout from './layout';
-import { metadata, viewport } from './metadata';
-
-export { metadata, viewport };
-
-export default function RootLayout({ children }) {
-  return <ClientLayout>{children}</ClientLayout>;
-} 
